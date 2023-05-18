a6f886581b57278a630c6bd47fea34df62e1cf92
Kevin Hawkins <khawkins@salesforce.com>
2023-05-17 21:38:45 -0700

    Initial wizard workflow
    - Starter Kit project instantiation, through Org authorization and deployment.
    - Clean up naming to be more consistent with VSCode, Salesforce conventions.

--- src/extension.ts
+++ src/extension.ts
@@ -1,84 +1,82 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { OnboardingCommands } from "./onboardingCommands";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const wizardCommand = "salesforcedx-vscode-offline-app.onboardingWizard";
+const onboardingWizardStateKey =
+  "com.salesforce.sandbox.offlineWizard.projectCreationState";
+
+enum OnboardingWizardState {
+  projectCreated,
+}
+
 export function activate(context: vscode.ExtensionContext) {
+  // If activation is coming as the result of the project being created and newly
+  // loaded into the workspace, pick up with the next step of the wizard.
+  const isPostProjectCreation =
+    context.globalState.get(onboardingWizardStateKey) ===
+    OnboardingWizardState.projectCreated;
+  if (isPostProjectCreation) {
+    context.globalState.update(onboardingWizardStateKey, undefined);
+    vscode.commands.executeCommand(wizardCommand, true);
+  }
+  let disposable = vscode.commands.registerCommand(
+    wizardCommand,
+    async (fromPostProjectCreation: boolean = false) => {
+      if (fromPostProjectCreation) {
+        await OnboardingCommands.deployToOrg();
+      } else {
+        const projectDir = await OnboardingCommands.configureProject(true);
+        if (projectDir === "") {
+          // No directory selected.
+          return Promise.resolve();
+        }
+        context.globalState.update(
+          onboardingWizardStateKey,
+          OnboardingWizardState.projectCreated
+        );
+      }
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('com.khawkins.sandbox.starterkit.wizard', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VSCode!');
-	});
-	context.subscriptions.push(disposable);
+  disposable = vscode.commands.registerCommand(
+    "com.salesforce.sandbox.starterkit.configureProject",
+    () => {
+      OnboardingCommands.configureProject();
+    }
+  );
+  context.subscriptions.push(disposable);
 
-	disposable = vscode.commands.registerCommand('com.khawkins.sandbox.starterkit.configureProject', async () => {
-		const header: vscode.QuickPickOptions = {
-			placeHolder: "Create a new project, or open an existing project"
-		};
-		let items: vscode.QuickPickItem[] = [
-			{
-				label: "Create New Project...",
-				description: "Creates a new local project configured with the Offline Starter Kit",
-			},
-			{
-				label: "Open Existing Project...",
-				description: "Opens an existing local project configured with the Offline Starter Kit",
-			},
-		];
-		const selected = await vscode.window.showQuickPick(items, header);
-		if (!selected) {
-			return;
-		}
+  disposable = vscode.commands.registerCommand(
+    "com.salesforce.sandbox.starterkit.deployProject",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello VSCode!");
+    }
+  );
+  context.subscriptions.push(disposable);
 
-		if (selected.label === "Create New Project..." ) {
-			const folderUri = await vscode.window.showOpenDialog({
-				openLabel: "Select project folder",
-				canSelectFolders: true,
-				canSelectFiles: false,
-				canSelectMany: false
-			});
-			if (!folderUri || folderUri.length === 0) {
-				return;
-			}
-			const githubRepoUri: string = "https://github.com/salesforce/offline-app-developer-starter-kit.git";
-			try {
-				await vscode.commands.executeCommand("git.clone", githubRepoUri, folderUri[0].fsPath);
-				vscode.window.showInformationMessage(`Cloned ${githubRepoUri} to ${folderUri[0].fsPath}`);
-			} catch (error) {
-				vscode.window.showErrorMessage(`Failed to clone: ${error}`);
-			}
-		} else if (selected.label === "Open Existing Project...") {
-			console.log("Open existing project");
-		}
+  disposable = vscode.commands.registerCommand(
+    "com.salesforce.sandbox.starterkit.landingPage",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello VSCode!");
+    }
+  );
+  context.subscriptions.push(disposable);
 
-	});
-	context.subscriptions.push(disposable);
-
-	disposable = vscode.commands.registerCommand('com.khawkins.sandbox.starterkit.deployProject', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VSCode!');
-	});
-	context.subscriptions.push(disposable);
-
-	disposable = vscode.commands.registerCommand('com.khawkins.sandbox.starterkit.landingPage', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VSCode!');
-	});
-	context.subscriptions.push(disposable);
-
-	disposable = vscode.commands.registerCommand('com.khawkins.sandbox.starterkit.sObjectSetup', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VSCode!');
-	});
-	context.subscriptions.push(disposable);
+  disposable = vscode.commands.registerCommand(
+    "com.salesforce.sandbox.starterkit.sObjectSetup",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello VSCode!");
+    }
+  );
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
---
+++ src/onboardingCommands.ts
@@ -0,0 +1,114 @@
+import * as vscode from "vscode";
+import * as path from "path";
+
+export class OnboardingCommands {
+  public static async configureProject(
+    fromWizard: boolean = false
+  ): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      const header: vscode.QuickPickOptions = {
+        placeHolder: "Create a new project, or open an existing project",
+      };
+      const items: vscode.QuickPickItem[] = [
+        {
+          label: "Create New Project...",
+          description:
+            "Creates a new local project configured with the Offline Starter Kit",
+        },
+        {
+          label: "Open Existing Project...",
+          description:
+            "Opens an existing local project configured with the Offline Starter Kit",
+        },
+      ];
+      const selected = await vscode.window.showQuickPick(items, header);
+      if (!selected) {
+        return resolve("");
+      }
+
+      if (selected.label === "Create New Project...") {
+        const folderUri = await vscode.window.showOpenDialog({
+          openLabel: "Select project folder",
+          canSelectFolders: true,
+          canSelectFiles: false,
+          canSelectMany: false,
+        });
+        if (!folderUri || folderUri.length === 0) {
+          return resolve("");
+        }
+
+        let infoMessage = "Follow the prompts to configure the project.";
+        if (fromWizard) {
+          infoMessage +=
+            " NOTE: after the project is loaded, please be patient while the wizard resumes.";
+        }
+        await vscode.window.showInformationMessage(infoMessage, {
+          title: "OK",
+        });
+        const githubRepoUri: string =
+          "https://github.com/salesforce/offline-app-developer-starter-kit.git";
+        try {
+          await vscode.commands.executeCommand(
+            "git.clone",
+            githubRepoUri,
+            folderUri[0].fsPath
+          );
+          return resolve(folderUri[0].fsPath);
+        } catch (error) {
+          vscode.window.showErrorMessage(`Failed to clone: ${error}`);
+          return reject(error);
+        }
+      } else if (selected.label === "Open Existing Project...") {
+        console.log("Open existing project");
+        return resolve("");
+      } else {
+        return resolve("");
+      }
+    });
+  }
+
+  public static async deployToOrg(): Promise<void> {
+    // TODO: Check to see if the Salesforce Extension Pack is installed.
+    // The commented code below starts the process.
+    // const extension = vscode.extensions.getExtension(
+    //   "salesforce.salesforcedx-vscode"
+    // );
+    // console.log(`Extension: ${extension}`);
+
+    const currentWorkspace = vscode.workspace;
+    if (!currentWorkspace.workspaceFolders) {
+      await vscode.window.showErrorMessage(
+        "There are no workspace folders defined in your project.",
+        { title: "OK" }
+      );
+      return Promise.resolve();
+    }
+
+    const result = await vscode.window.showInformationMessage(
+      "Do you want to authorize an Org, or deploy to an already-authorized Org?",
+      { title: "Authorize" },
+      { title: "Deploy" }
+    );
+
+    if (result) {
+      if (result.title === "Authorize") {
+        await vscode.commands.executeCommand("sfdx.force.auth.web.login");
+        await vscode.window.showInformationMessage(
+          "Once you've authorized your Org, click here to continue.",
+          { title: "OK" }
+        );
+      }
+      const workspaceFolderPath =
+        currentWorkspace.workspaceFolders[0].uri.fsPath;
+      const forceAppPath = path.join(workspaceFolderPath, "force-app");
+      const forceAppUri = vscode.Uri.file(forceAppPath);
+      await vscode.commands.executeCommand(
+        "sfdx.force.source.deploy.source.path",
+        forceAppUri
+      );
+      return Promise.resolve();
+    } else {
+      return Promise.resolve();
+    }
+  }
+}
