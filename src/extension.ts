9feb728efa124a4c77792e4b84f3c5c0d757e07f
Takashi Arai <t.arai@salesforce.com>
2023-06-01 07:00:24 -1000

    Copyright header.

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
--- src/onboardingCommands.ts
+++ src/onboardingCommands.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
 import * as vscode from "vscode";
 import * as path from "path";
 import * as fs from "fs";
