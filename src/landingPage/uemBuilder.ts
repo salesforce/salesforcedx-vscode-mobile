541cf72ca29fa440a4a517f0be79f542ca9b983b
Takashi Arai <t.arai@salesforce.com>
2023-06-05 10:43:29 -0700

    Add prettier.

--- src/landingPage/uemBuilder.ts
+++ src/landingPage/uemBuilder.ts
@@ -1,27 +1,37 @@
-import { messages } from "../messages/messages";
+import { messages } from '../messages/messages';
 
 export class UEMBuilder {
     cards: any[] = [];
 
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    static readonly GLOBAL_ACTIONS_LABEL = messages.getMessage('card_name_global_actions');
+    static readonly GLOBAL_ACTIONS_LABEL = messages.getMessage(
+        'card_name_global_actions'
+    );
 
     addGlobalActionCard(): UEMBuilder {
         const listUEM = {
-            definition: "mcfp/actionList",
-            name: "actions_list",
+            definition: 'mcfp/actionList',
+            name: 'actions_list',
             label: UEMBuilder.GLOBAL_ACTIONS_LABEL,
             properties: {},
-            regions: {},
+            regions: {}
         };
 
-        this.cards.push(this.cardUEM("global_actions", UEMBuilder.GLOBAL_ACTIONS_LABEL, listUEM));
+        this.cards.push(
+            this.cardUEM(
+                'global_actions',
+                UEMBuilder.GLOBAL_ACTIONS_LABEL,
+                listUEM
+            )
+        );
         return this;
     }
 
     build(): any {
         let uem = this.uemTemplate();
-        const cmps = uem.view.regions.components.components[0].regions.components.components;
+        const cmps =
+            uem.view.regions.components.components[0].regions.components
+                .components;
         for (const card of this.cards) {
             cmps.push(card);
         }
@@ -30,48 +40,48 @@ export class UEMBuilder {
 
     private cardUEM(name: string, label: string, ...children: any[]): any {
         return {
-            definition: "mcf/card",
+            definition: 'mcf/card',
             name: name,
             properties: { label },
             regions: {
                 components: {
-                    name: "components",
-                    components: children,
-                },
-            },
+                    name: 'components',
+                    components: children
+                }
+            }
         };
     }
 
     private uemTemplate(): any {
         return {
             view: {
-                definition: "generated/uem_output",
-                name: "view",
+                definition: 'generated/uem_output',
+                name: 'view',
                 properties: {},
                 regions: {
                     components: {
-                        name: "components",
+                        name: 'components',
                         components: [
                             {
-                                definition: "mcf/container",
-                                name: "container",
+                                definition: 'mcf/container',
+                                name: 'container',
                                 properties: {
-                                    backgroundColor: "#F3F2f2",
+                                    backgroundColor: '#F3F2f2'
                                 },
                                 regions: {
                                     components: {
-                                        name: "components",
+                                        name: 'components',
                                         components: [] as any[]
-                                    },
-                                },
-                            },
-                        ],
-                    },
-                },
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                }
             },
-            target: "mcf__native",
-            apiName: "landing_page",
-            id: "611ca737-feb2-41c8-8c87-4f48a3415e3b",
+            target: 'mcf__native',
+            apiName: 'landing_page',
+            id: '611ca737-feb2-41c8-8c87-4f48a3415e3b'
         };
     }
 }
--- src/onboardingCommands.ts
+++ src/onboardingCommands.ts
@@ -8,139 +8,158 @@
 import * as vscode from "vscode";
 import * as path from "path";
 import { CommonUtils } from "@salesforce/lwc-dev-mobile-core/lib/common/CommonUtils";
+import { InstructionsWebviewProvider } from "./webviews";
 import { showInstructionWebView } from "./extension";
 
 export class OnboardingCommands {
-  public static async configureProject(
-    fromWizard: boolean = false
-  ): Promise<string> {
-    return new Promise(async (resolve, reject) => {
-      const header: vscode.QuickPickOptions = {
-        placeHolder: "Create a new project, or open an existing project",
-      };
-      const items: vscode.QuickPickItem[] = [
-        {
-          label: "Create New Project...",
-          description:
-            "Creates a new local project configured with the Offline Starter Kit",
-        },
-        {
-          label: "Open Existing Project...",
-          description:
-            "Opens an existing local project configured with the Offline Starter Kit",
-        },
-      ];
-      const selected = await vscode.window.showQuickPick(items, header);
-      if (!selected) {
-        return resolve("");
-      }
+    public static async configureProject(
+        fromWizard: boolean = false
+    ): Promise<string> {
+        return new Promise(async (resolve, reject) => {
+            const header: vscode.QuickPickOptions = {
+                placeHolder: 'Create a new project, or open an existing project'
+            };
+            const items: vscode.QuickPickItem[] = [
+                {
+                    label: 'Create New Project...',
+                    description:
+                        'Creates a new local project configured with the Offline Starter Kit'
+                },
+                {
+                    label: 'Open Existing Project...',
+                    description:
+                        'Opens an existing local project configured with the Offline Starter Kit'
+                }
+            ];
+            const selected = await vscode.window.showQuickPick(items, header);
+            if (!selected) {
+                return resolve('');
+            }
 
-      if (selected.label === "Create New Project...") {
-        const folderUri = await vscode.window.showOpenDialog({
-          openLabel: "Select project folder",
-          canSelectFolders: true,
-          canSelectFiles: false,
-          canSelectMany: false,
+            if (selected.label === 'Create New Project...') {
+                const folderUri = await vscode.window.showOpenDialog({
+                    openLabel: 'Select project folder',
+                    canSelectFolders: true,
+                    canSelectFiles: false,
+                    canSelectMany: false
+                });
+                if (!folderUri || folderUri.length === 0) {
+                    return resolve('');
+                }
+
+                let infoMessage =
+                    'Follow the prompts to configure the project.';
+                if (fromWizard) {
+                    infoMessage +=
+                        ' NOTE: after the project is loaded, please be patient while the wizard resumes.';
+                }
+                await vscode.window.showInformationMessage(infoMessage, {
+                    title: 'OK'
+                });
+                const githubRepoUri: string =
+                    'https://github.com/salesforce/offline-app-developer-starter-kit.git';
+                try {
+                    await vscode.commands.executeCommand(
+                        'git.clone',
+                        githubRepoUri,
+                        folderUri[0].fsPath
+                    );
+                    return resolve(folderUri[0].fsPath);
+                } catch (error) {
+                    vscode.window.showErrorMessage(`Failed to clone: ${error}`);
+                    return reject(error);
+                }
+            } else if (selected.label === 'Open Existing Project...') {
+                console.log('Open existing project');
+                return resolve('');
+            } else {
+                return resolve('');
+            }
         });
-        if (!folderUri || folderUri.length === 0) {
-          return resolve("");
-        }
-
-        let infoMessage = "Follow the prompts to configure the project.";
-        if (fromWizard) {
-          infoMessage +=
-            " NOTE: after the project is loaded, please be patient while the wizard resumes.";
-        }
-        await vscode.window.showInformationMessage(infoMessage, {
-          title: "OK",
-        });
-        const githubRepoUri: string =
-          "https://github.com/salesforce/offline-app-developer-starter-kit.git";
-        try {
-          await vscode.commands.executeCommand(
-            "git.clone",
-            githubRepoUri,
-            folderUri[0].fsPath
-          );
-          return resolve(folderUri[0].fsPath);
-        } catch (error) {
-          vscode.window.showErrorMessage(`Failed to clone: ${error}`);
-          return reject(error);
-        }
-      } else if (selected.label === "Open Existing Project...") {
-        console.log("Open existing project");
-        return resolve("");
-      } else {
-        return resolve("");
-      }
-    });
-  }
-
-  public static async deployToOrg(): Promise<void> {
-    // TODO: Check to see if the Salesforce Extension Pack is installed.
-    // The commented code below starts the process.
-    // const extension = vscode.extensions.getExtension(
-    //   "salesforce.salesforcedx-vscode"
-    // );
-    // console.log(`Extension: ${extension}`);
-
-    const currentWorkspace = vscode.workspace;
-    if (!currentWorkspace.workspaceFolders) {
-      await vscode.window.showErrorMessage(
-        "There are no workspace folders defined in your project.",
-        { title: "OK" }
-      );
-      return Promise.resolve();
     }
 
-    const result = await vscode.window.showInformationMessage(
-      "Do you want to authorize an Org, or deploy to an already-authorized Org?",
-      { title: "Authorize" },
-      { title: "Deploy" }
-    );
+    public static async deployToOrg(): Promise<void> {
+        // TODO: Check to see if the Salesforce Extension Pack is installed.
+        // The commented code below starts the process.
+        // const extension = vscode.extensions.getExtension(
+        //   "salesforce.salesforcedx-vscode"
+        // );
+        // console.log(`Extension: ${extension}`);
 
-    if (result) {
-      if (result.title === "Authorize") {
-        await vscode.commands.executeCommand("sfdx.force.auth.web.login");
+        const currentWorkspace = vscode.workspace;
+        if (!currentWorkspace.workspaceFolders) {
+            await vscode.window.showErrorMessage(
+                'There are no workspace folders defined in your project.',
+                { title: 'OK' }
+            );
+            return Promise.resolve();
+        }
+
+        const result = await vscode.window.showInformationMessage(
+            'Do you want to authorize an Org, or deploy to an already-authorized Org?',
+            { title: 'Authorize' },
+            { title: 'Deploy' }
+        );
+
+        if (result) {
+            if (result.title === 'Authorize') {
+                await vscode.commands.executeCommand(
+                    'sfdx.force.auth.web.login'
+                );
+                await vscode.window.showInformationMessage(
+                    "Once you've authorized your Org, click here to continue.",
+                    { title: 'OK' }
+                );
+            }
+            const workspaceFolderPath =
+                currentWorkspace.workspaceFolders[0].uri.fsPath;
+            const forceAppPath = path.join(workspaceFolderPath, 'force-app');
+            const forceAppUri = vscode.Uri.file(forceAppPath);
+            await vscode.commands.executeCommand(
+                'sfdx.force.source.deploy.source.path',
+                forceAppUri
+            );
+            return Promise.resolve();
+        } else {
+            return Promise.resolve();
+        }
+    }
+
+    public static async setupBriefcase(
+        extensionUri: vscode.Uri
+    ): Promise<void> {
         await vscode.window.showInformationMessage(
-          "Once you've authorized your Org, click here to continue.",
-          { title: "OK" }
+            'Click OK to launch your org to the Briefcase Builder page. After ' +
+                'launching, return here for instructions to set up a Briefcase rule.',
+            { title: 'OK' }
         );
-      }
-      const workspaceFolderPath =
-        currentWorkspace.workspaceFolders[0].uri.fsPath;
-      const forceAppPath = path.join(workspaceFolderPath, "force-app");
-      const forceAppUri = vscode.Uri.file(forceAppPath);
-      await vscode.commands.executeCommand(
-        "sfdx.force.source.deploy.source.path",
-        forceAppUri
-      );
-      return Promise.resolve();
-    } else {
-      return Promise.resolve();
+
+        // TODO: this `withProgress` call probably needs tweaking on UX.
+        await vscode.window.withProgress(
+            {
+                location: vscode.ProgressLocation.Notification,
+                title: 'Launching Briefcase Builder...'
+            },
+            async (progress, token) => {
+                await CommonUtils.executeCommandAsync(
+                    "sfdx org open -p '/lightning/setup/Briefcase/home'"
+                );
+            }
+        );
+
+        const provider: InstructionsWebviewProvider =
+            new InstructionsWebviewProvider(extensionUri);
+        provider.showInstructionWebview(
+            'Briefcase Setup Instructions',
+            'src/instructions/briefcase.html',
+            [
+                {
+                    buttonId: 'okButton',
+                    action: (panel) => {
+                        panel.dispose();
+                    }
+                }
+            ]
+        );
     }
-  }
-
-  public static async setupBriefcase(extensionUri: vscode.Uri): Promise<void> {
-    await vscode.window.showInformationMessage(
-      "Click OK to launch your org to the Briefcase Builder page. After " +
-        "launching, return here for instructions to set up a Briefcase rule.",
-      { title: "OK" }
-    );
-
-    // TODO: this `withProgress` call probably needs tweaking on UX.
-    await vscode.window.withProgress(
-      {
-        location: vscode.ProgressLocation.Notification,
-        title: "Launching Briefcase Builder...",
-      },
-      async (progress, token) => {
-        await CommonUtils.executeCommandAsync(
-          "sfdx org open -p '/lightning/setup/Briefcase/home'"
-        );
-      }
-    );
-
-    showInstructionWebView(extensionUri, "Briefcase Setup Instruction", "src/instructions/briefcase.html");
-  }
 }
