098df23179747d7d5f1f87214553aa22e0397b9f
Takashi Arai <t.arai@salesforce.com>
2023-06-09 01:17:59 -0700

    nit.

--- src/commands/authorizeCommand.ts
+++ src/commands/authorizeCommand.ts
@@ -1,9 +1,9 @@
-// /*
-//  * Copyright (c) 2023, salesforce.com, inc.
-//  * All rights reserved.
-//  * SPDX-License-Identifier: MIT
-//  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
-//  */
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
 
 import { commands, window } from 'vscode';
 import { OrgUtils } from '../utils/orgUtils';
