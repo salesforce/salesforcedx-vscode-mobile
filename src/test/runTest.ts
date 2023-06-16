a0a420cff6edaacd08e36c38f1dbb96cccb5b21f
Takashi Arai <t.arai@salesforce.com>
2023-06-15 22:35:46 -0700

    Use NYC to instrument programatically.

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -8,6 +8,19 @@
 import * as path from 'path';
 
 import { runTests } from '@vscode/test-electron';
+import { instrument } from './suite/coverage';
+
+const NYC = require('nyc');
+import * as glob from 'glob';
+
+// Simulates the recommended config option
+// extends: "@istanbuljs/nyc-config-typescript",
+import * as baseConfig from '@istanbuljs/nyc-config-typescript';
+
+// Recommended modules, loading them here to speed up NYC init
+// and minimize risk of race condition
+import 'ts-node/register';
+import 'source-map-support/register';
 
 async function main() {
     try {
@@ -17,7 +30,40 @@ async function main() {
 
         // The path to test runner
         // Passed to --extensionTestsPath
-        const extensionTestsPath = path.resolve(__dirname, './suite/index');
+        
+        //const extensionTestsPath = path.resolve(__dirname, './suite/index');
+        let extensionTestsPath = path.resolve(__dirname, './suite/index');
+
+        // if (process.argv.indexOf('--coverage') >= 0) {
+        //     // generate instrumented files at out-cov
+        //     //instrument();
+        //     const nyc = new NYC({
+        //         ...baseConfig,
+        //         //cwd: path.join(__dirname, '..', '..', '..'),
+        //         cwd: path.join(__dirname, '..', '..'),
+        //         reporter: ['text-summary', 'html', 'text'],
+        //         all: true,
+        //         silent: false,
+        //         instrument: true,
+        //         hookRequire: true,
+        //         //hookRunInContext: true,
+        //         // hookRunInThisContext: true,
+        //         include: ['out/**/*.js'],
+        //         // exclude: ['out/test/**', 'out/test/*']
+        //         //tempDir: 'foo' 
+        //     });
+        //     //await nyc.reset();
+        //     await nyc.wrap();
+        //     console.error('Glob verification', await nyc.exclude.glob(nyc.cwd));
+
+      
+        //     // load the instrumented files
+        //     //extensionTestsPath = path.resolve(__dirname, '../out-cov/test/suite/index');
+        //     extensionTestsPath = path.resolve(__dirname, '../test/suite/index');
+      
+        //     // signal that the coverage data should be gathered
+        //     process.env['GENERATE_COVERAGE'] = '1';
+        // }
 
         // Download VS Code, unzip it and run the integration test
         await runTests({ extensionDevelopmentPath, extensionTestsPath });
--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -5,41 +5,114 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 
+'use strict';
+
 import * as path from 'path';
 import * as Mocha from 'mocha';
+const NYC = require('nyc');
 import * as glob from 'glob';
 
-export function run(): Promise<void> {
+// // Simulates the recommended config option
+// // extends: "@istanbuljs/nyc-config-typescript",
+import * as baseConfig from '@istanbuljs/nyc-config-typescript';
+
+// Recommended modules, loading them here to speed up NYC init
+// and minimize risk of race condition
+import 'ts-node/register';
+import 'source-map-support/register';
+
+export async function run(): Promise<void> {
+    const testsRoot = path.resolve(__dirname, '..');
+
+    // Setup coverage pre-test, including post-test hook to report
+
+    const nyc = new NYC({
+        ...baseConfig,
+        cwd: path.join(__dirname, '..', '..', '..'),
+        reporter: ['text-summary', 'html', 'text'],
+        all: true,
+        silent: false,
+        instrument: true,
+        hookRequire: true,
+        //hookRunInContext: true,
+        // hookRunInThisContext: true,
+        include: ['out/**/*.js'],
+        // exclude: ['out/test/**', 'out/test/*']
+    });
+    // await nyc.reset();
+    await nyc.wrap();
+
+    // Object.keys(require.cache).filter(f => nyc.exclude.shouldInstrument(f)).forEach(m => {
+    //     console.warn('Module loaded before NYC, invalidating:', m);
+    //     delete require.cache[m];
+    //     require(m);
+    //   });
+
+     // Debug which files will be included/excluded
+   console.error('Glob verification', await nyc.exclude.glob(nyc.cwd));
+
     // Create the mocha test
     const mocha = new Mocha({
         ui: 'tdd',
+        // timeout: 10 * 1000
         color: true
     });
 
-    const testsRoot = path.resolve(__dirname, '..');
+    
+    // return new Promise((c, e) => {
+    //     glob('**/**.test.js', { cwd: testsRoot }, async (err, files) => {
+    //         if (err) {
+    //             return e(err);
+    //         }
 
-    return new Promise((c, e) => {
-        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-            if (err) {
-                return e(err);
-            }
+    //         // Add files to the test suite
+    //         files.forEach((f) => {
+    //             const pathName = path.resolve(testsRoot, f);
+    //             console.error(pathName);
 
-            // Add files to the test suite
-            files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+    //             mocha.addFile(path.resolve(testsRoot, f));
 
-            try {
-                // Run the mocha test
-                mocha.run((failures) => {
-                    if (failures > 0) {
-                        e(new Error(`${failures} tests failed.`));
-                    } else {
-                        c();
-                    }
-                });
-            } catch (err) {
-                console.error(err);
-                e(err);
-            }
-        });
-    });
+    //         });
+
+    //         try {
+    //             // Run the mocha test
+    //             mocha.run( async (failures) => {
+    //                 if (failures > 0) {
+    //                     e(new Error(`${failures} tests failed.`));
+    //                 } else {
+    //                     c();
+    //                 }
+    //             });
+    //         } catch (err) {
+    //             console.error(err);
+    //             e(err);
+    //         } 
+    //     });
+    // });
+
+    // Add all files to the test suite
+    const files = glob.sync('**/*.test.js', { cwd: testsRoot });
+    files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+
+    const failures: number = await new Promise((resolve) => mocha.run(resolve));
+    await nyc.writeCoverageFile();
+
+    // Capture text-summary reporter's output and log it in console
+    console.log(await captureStdout(nyc.report.bind(nyc)));
+
+    if (failures > 0) {
+        throw new Error(`${failures} tests failed.`);
+    }
+}
+
+async function captureStdout(fn: any) {
+    let w = process.stdout.write,
+        buffer = '';
+    process.stdout.write = (s) => {
+        buffer = buffer + s;
+        return true;
+    };
+    await fn();
+    process.stdout.write = w;
+    return buffer;
 }
