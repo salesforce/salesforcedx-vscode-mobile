e0a0649e28b867bd9e8ee6d06e5e208f4d26f289
Takashi Arai <t.arai@salesforce.com>
2023-06-17 10:45:49 -0700

    Cleanup.

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -11,6 +11,7 @@ import * as Mocha from 'mocha';
 import * as path from 'path';
 import * as glob from 'glob';
 import * as baseConfig from '@istanbuljs/nyc-config-typescript';
+
 const NYC = require('nyc');
 
 export async function run(): Promise<void> {
@@ -26,28 +27,16 @@ export async function run(): Promise<void> {
         silent: false,
         instrument: true,
         hookRequire: true,
-        // hookRunInContext: true,
-        // hookRunInThisContext: true,
-        // useSpawnWrap: false,
         include: ['out/**/*.js'],
         exclude: ['out/test/**'],
     });
     await nyc.wrap();
 
-    // Object.keys(require.cache)
-    // .filter(f => nyc.exclude.shouldInstrument(f) )
-    // .forEach(m => {
-    //     console.warn('Module loaded before NYC, invalidatingaaa:', m);
-    //     //delete require.cache[m];
-    //     decache(m);
-    //     require(m);
-    //   });
-
-    const myFilesRegex = /salesforce-offline-vscode\/out/;
-  const filterFn = myFilesRegex.test.bind(myFilesRegex);
-  if (Object.keys(require.cache).filter(filterFn).length > 1) {
-    console.warn('NYC initialized after modules were loaded', Object.keys(require.cache).filter(filterFn));
-  }
+//     const myFilesRegex = /salesforce-offline-vscode\/out/;
+//       const filterFn = myFilesRegex.test.bind(myFilesRegex);
+//   if (Object.keys(require.cache).filter(filterFn).length > 1) {
+//     console.warn('NYC initialized after modules were loaded', Object.keys(require.cache).filter(filterFn));
+//   }
   await nyc.createTempDirectory();
 
     // Create the mocha test
@@ -57,60 +46,54 @@ export async function run(): Promise<void> {
         color: true
     });
 
-    // return new Promise((c, e) => {
-    //     glob('**/**.test.js', { cwd: testsRoot }, async (err, files) => {
-    //         if (err) {
-    //             return e(err);
-    //         }
+    return new Promise((c, e) => {
+        glob('**/**.test.js', { cwd: testsRoot }, async (err, files) => {
+            if (err) {
+                return e(err);
+            }
 
-    //         // Add files to the test suite
-    //         files.forEach((f) => {
-    //             const pathName = path.resolve(testsRoot, f);
-    //             console.error(pathName);
+            // Add files to the test suite
+            files.forEach((f) => {
+                mocha.addFile(path.resolve(testsRoot, f));
+            });
 
-    //             mocha.addFile(path.resolve(testsRoot, f));
+            try {
+                // Run the mocha test
+                mocha.run(async (failures) => {
+                    if (failures > 0) {
+                        e(new Error(`${failures} tests failed.`));
+                    } else {
+                        await nyc.writeCoverageFile();
 
-    //         });
-
-    //         try {
-    //             // Run the mocha test
-    //             mocha.run( async (failures) => {
-    //                 if (failures > 0) {
-    //                     e(new Error(`${failures} tests failed.`));
-    //                 } else {
-    //                     c();
-    //                 }
-    //             });
-    //         } catch (err) {
-    //             console.error(err);
-    //             e(err);
-    //         }
-    //     });
-    // });
-
-    // Add all files to the test suite
-    const files = glob.sync('**/*.test.js', { cwd: testsRoot });
-    files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
-
-    const failures: number = await new Promise((resolve) => mocha.run(resolve));
-    await nyc.writeCoverageFile();
-
-    // Capture text-summary reporter's output and log it in console
-    console.log(await captureStdout(nyc.report.bind(nyc)));
-
-    if (failures > 0) {
-        throw new Error(`${failures} tests failed.`);
-    }
+                        // nyc text report is output to process.stdout and using plain console.log
+                        // will not output to terminal. Overriding process.stdout to pipe the output
+                        // to console.log.
+                        console.log(await pipeNycReport(nyc.report.bind(nyc)));
+                        c();
+                    }
+                });
+            } catch (err) {
+                console.error(err);
+                e(err);
+            }
+        });
+    });
 }
 
-async function captureStdout(fn: any) {
-    let w = process.stdout.write,
-        buffer = '';
-    process.stdout.write = (s) => {
-        buffer = buffer + s;
+async function pipeNycReport(callback: any):Promise<string> {
+    // Preserve original write function
+    let oldWriteFunc = process.stdout.write;
+
+    // Create a string buffer to stash report output
+    let reportText = '';
+    process.stdout.write = (text) => {
+        reportText = reportText.concat(text);
         return true;
     };
-    await fn();
-    process.stdout.write = w;
-    return buffer;
+    await callback();
+
+    // Revert to original write function
+    process.stdout.write = oldWriteFunc;
+
+    return Promise.resolve(reportText);
 }
