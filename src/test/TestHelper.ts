8d4f2c5b35a9cea0cd002b139264a6da533a255f
Kevin Hawkins <khawkins@salesforce.com>
2023-06-20 16:25:20 -0700

    Project cleanup

--- src/test/TestHelper.ts
+++ src/test/TestHelper.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
 import { Stats } from 'fs';
 import { mkdtemp, rm, stat } from 'fs/promises';
 import * as os from 'os';
