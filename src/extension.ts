52c3afe25ad221970ebece73c85b6f7a651fc645
Kevin Hawkins <khawkins@salesforce.com>
2023-06-22 21:53:12 -0700

    Content updates, bug fix

--- src/extension.ts
+++ src/extension.ts
@@ -20,50 +20,73 @@ const onboardingWizardStateKey =
     'salesforcedx-vscode-offline-app.onboardingWizard.projectCreationState';
 
 enum OnboardingWizardState {
-    projectCreated
+    projectConfigured
+}
+
+async function runPostProjectConfigurationSteps(
+    extensionUri: vscode.Uri
+): Promise<void> {
+    return new Promise(async (resolve) => {
+        await AuthorizeCommand.authorizeToOrg();
+        await BriefcaseCommand.setupBriefcase(extensionUri);
+        await TemplateChooserCommand.copyDefaultTemplate(extensionUri);
+
+        await AuthorizeCommand.authorizeToOrg();
+        await DeployToOrgCommand.deployToOrg();
+
+        await InstructionsWebviewProvider.showDismissableInstructions(
+            extensionUri,
+            vscode.l10n.t('Salesforce Mobile App Instructions'),
+            'src/instructions/salesforcemobileapp.html'
+        );
+        return resolve();
+    });
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    // If activation is coming as the result of the project being created and newly
+    // If activation is coming as the result of the project being newly
     // loaded into the workspace, pick up with the next step of the wizard.
-    const isPostProjectCreation =
+    const isPostProjectConfiguration =
         context.globalState.get(onboardingWizardStateKey) ===
-        OnboardingWizardState.projectCreated;
-    if (isPostProjectCreation) {
+        OnboardingWizardState.projectConfigured;
+    if (isPostProjectConfiguration) {
         context.globalState.update(onboardingWizardStateKey, undefined);
         vscode.commands.executeCommand(wizardCommand, true);
     }
     vscode.commands.registerCommand(
         wizardCommand,
-        async (fromPostProjectCreation: boolean = false) => {
-            if (fromPostProjectCreation) {
-                await AuthorizeCommand.authorizeToOrg();
-                await BriefcaseCommand.setupBriefcase(context.extensionUri);
-                await TemplateChooserCommand.copyDefaultTemplate(
-                    context.extensionUri
-                );
-
-                await AuthorizeCommand.authorizeToOrg();
-                await DeployToOrgCommand.deployToOrg();
-
-                InstructionsWebviewProvider.showDismissableInstructions(
-                    context.extensionUri,
-                    vscode.l10n.t('Salesforce Mobile App Instructions'),
-                    'src/instructions/salesforcemobileapp.html'
-                );
+        async (fromPostProjectConfiguration: boolean = false) => {
+            if (fromPostProjectConfiguration) {
+                await runPostProjectConfigurationSteps(context.extensionUri);
             } else {
                 const projectDir = await new ConfigureProjectCommand(
                     context.extensionUri
                 ).configureProject();
                 if (!projectDir) {
-                    // No directory selected.
+                    // No directory selected. Do not continue.
                     return Promise.resolve();
+                } else if (
+                    vscode.workspace.workspaceFolders &&
+                    vscode.workspace.workspaceFolders.length > 0 &&
+                    vscode.workspace.workspaceFolders[0].uri.fsPath ===
+                        projectDir
+                ) {
+                    // Selected folder is already loaded into the workspace.
+                    // Run the next steps directly, because the workspace will
+                    // not reload in this case.
+                    await runPostProjectConfigurationSteps(
+                        context.extensionUri
+                    );
+                } else {
+                    // Different project folder from what's currently loaded
+                    // into the workspace. The workspace will reload,
+                    // and we need to set a breadcrumb to pick up with the
+                    // next steps, after it does.
+                    context.globalState.update(
+                        onboardingWizardStateKey,
+                        OnboardingWizardState.projectConfigured
+                    );
                 }
-
-                context.globalState.update(
-                    onboardingWizardStateKey,
-                    OnboardingWizardState.projectCreated
-                );
             }
         }
     );
