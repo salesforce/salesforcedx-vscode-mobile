86f03e90b8d7d93595cab2d3824b3972cd1d36f3
Ben Zhang <ben.zhang@salesforce.com>
2024-06-24 17:38:26 -0700

    feat: config lwc mobile eslint plugin

--- src/commands/lint/configureLintingToolsCommand.ts
+++ src/commands/lint/configureLintingToolsCommand.ts
@@ -11,29 +11,46 @@ import * as path from 'path';
 import { WorkspaceUtils } from '../../utils/workspaceUtils';
 import { JSON_INDENTATION_SPACES } from '../../utils/constants';
 
-const config = workspace.getConfiguration();
-const eslintPluginLwcGraphAnalyzer =
-    '@salesforce/eslint-plugin-lwc-graph-analyzer';
-const eslintPluginLwcGraphAnalyzerConfig =
-    'mobileOfflineLinting.eslint-plugin-lwc-graph-analyzer';
-const eslintPluginLwcGraphAnalyzerVersion = config.get(
-    eslintPluginLwcGraphAnalyzerConfig
-) as string;
-
-const eslint = 'eslint';
-const eslintConfig = 'mobileOfflineLinting.eslint';
-const eslintVersion = config.get(eslintConfig) as string;
-
 const configureLintingToolsCommand =
     'salesforcedx-vscode-offline-app.configureLintingTools';
-const eslintDependencies = [
-    [eslintPluginLwcGraphAnalyzer, eslintPluginLwcGraphAnalyzerVersion],
-    [eslint, eslintVersion]
-];
 
-const lwcGraphAnalyzerRecommended: string =
-    'plugin:@salesforce/lwc-graph-analyzer/recommended';
-const eslintRecommended = 'eslint:recommended';
+const config = workspace.getConfiguration();
+
+class EslintDependencyConfig {
+    readonly name: string;
+    readonly packageJsonPropertyId: string;
+    readonly version: string;
+    readonly eslintrcExtend: string;
+
+    constructor(
+        name: string,
+        packageJsonPropertyId: string,
+        eslintrcExtend: string
+    ) {
+        this.name = name;
+        this.packageJsonPropertyId = packageJsonPropertyId;
+        this.eslintrcExtend = eslintrcExtend;
+        this.version = config.get(this.packageJsonPropertyId) as string;
+    }
+}
+
+const eslintDependencies: EslintDependencyConfig[] = [
+    new EslintDependencyConfig(
+        '@salesforce/eslint-plugin-lwc-mobile',
+        'mobileOfflineLinting.eslint-plugin-lwc-mobile',
+        'plugin:@salesforce/lwc-mobile/recommended'
+    ),
+    new EslintDependencyConfig(
+        '@salesforce/eslint-plugin-lwc-graph-analyzer',
+        'mobileOfflineLinting.eslint-plugin-lwc-graph-analyzer',
+        'plugin:@salesforce/lwc-graph-analyzer/recommended'
+    ),
+    new EslintDependencyConfig(
+        'eslint',
+        'mobileOfflineLinting.eslint',
+        'eslint:recommended'
+    )
+];
 
 interface PackageJson {
     devDependencies?: Record<string, string>;
@@ -135,10 +152,10 @@ export class ConfigureLintingToolsCommand {
         let modified = false;
 
         if (devDependencies) {
-            eslintDependencies.forEach((nameValuePair) => {
-                const [name, value] = nameValuePair;
+            eslintDependencies.forEach((dependencyConfig) => {
+                const { name, version } = dependencyConfig;
                 if (!devDependencies[name]) {
-                    devDependencies[name] = value;
+                    devDependencies[name] = version;
                     modified = true;
                 }
             });
@@ -170,15 +187,12 @@ export class ConfigureLintingToolsCommand {
 
             let modified = false;
 
-            if (!eslintrcExtends.includes(eslintRecommended)) {
-                eslintrcExtends.push(eslintRecommended);
-                modified = true;
-            }
-
-            if (!eslintrcExtends.includes(lwcGraphAnalyzerRecommended)) {
-                eslintrc.extends.push(lwcGraphAnalyzerRecommended);
-                modified = true;
-            }
+            eslintDependencies.forEach((config) => {
+                if (!eslintrcExtends.includes(config.eslintrcExtend)) {
+                    eslintrcExtends.push(config.eslintrcExtend);
+                    modified = true;
+                }
+            });
 
             if (modified) {
                 // Save json only if the content was modified.
@@ -192,11 +206,11 @@ export class ConfigureLintingToolsCommand {
         } else {
             // Create eslintrc
             const eslintrc = {
-                extends: [
-                    `${eslintRecommended}`,
-                    `${lwcGraphAnalyzerRecommended}`
-                ]
+                extends: eslintDependencies.map((config) => {
+                    return `${config.eslintrcExtend}`;
+                })
             };
+
             const jsonString = JSON.stringify(
                 eslintrc,
                 null,
