6296776bc42f7decbd04a2f458786aa1e7b469fd
haifeng.li <haifeng.li@salesforce.com>
2024-10-21 16:14:10 -0700

    chore: add copyright

--- lsp/server/src/types/ObjectInfoRepresentation.ts
+++ lsp/server/src/types/ObjectInfoRepresentation.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2024, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
 import type { FieldRepresentation } from './FieldRepresentation';
 export interface ObjectInfoRepresentation {
     apiName: string;
