5f8996fc5bd7e9fb33de2cb391533b0b70e58eb1
Haifeng Li <168469994+haifeng-li-at-salesforce@users.noreply.github.com>
2024-11-01 18:18:50 -0700

    Validator pattern change (#148)
    feat: implement ValidatorManager

--- lsp/server/src/server.ts
+++ lsp/server/src/server.ts
@@ -19,10 +19,10 @@ import {
     CodeActionKind
 } from 'vscode-languageserver/node';
 import { TextDocument } from 'vscode-languageserver-textdocument';
-import { validateDocument } from './validateDocument';
 import { OrgUtils } from './utils/orgUtils';
 import { WorkspaceUtils } from './utils/workspaceUtils';
 import { getSettings } from './diagnostic/DiagnosticSettings';
+import { ValidatorManager } from './validatorManager';
 import { debounce } from './utils/commonUtils';
 
 // Create a connection for the server, using Node's IPC as a transport.
@@ -41,6 +41,9 @@ let diagnosticsSettingSection = '';
 
 // initialize default settings
 let settings = getSettings({});
+
+const validatorManager = ValidatorManager.createInstance();
+
 const documentCache: Map<string, TextDocument> = new Map();
 
 connection.onInitialize((params: InitializeParams) => {
@@ -172,7 +175,11 @@ connection.languages.diagnostics.on(async (params) => {
     if (document !== undefined) {
         return {
             kind: DocumentDiagnosticReportKind.Full,
-            items: await validateDocument(settings, document, extensionTitle)
+            items: await validatorManager.validateDocument(
+                settings,
+                document,
+                extensionTitle
+            )
         } satisfies DocumentDiagnosticReport;
     } else {
         // We don't know the document. We can either try to read it from disk
---
+++ lsp/server/src/validator/jsValidator.ts
@@ -0,0 +1,33 @@
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import { BaseValidator } from './baseValidator';
+import type { Node } from '@babel/types';
+import { parseJs } from '../utils/babelUtil';
+import { DiagnosticSection } from './baseValidator';
+
+export class JSValidator extends BaseValidator<Node> {
+    gatherDiagnosticSections(
+        textDocument: TextDocument
+    ): DiagnosticSection<Node>[] {
+        try {
+            const data = parseJs(textDocument.getText());
+            //One DiagnosticSection with lineOffset and columnOffset as 0
+            return [
+                {
+                    data,
+                    document: textDocument,
+                    lineOffset: 0,
+                    columnOffset: 0
+                } satisfies DiagnosticSection<Node>
+            ];
+        } catch (e) {
+            console.log(
+                `Failed to parse JavaScript file: : ${(e as Error).message}`
+            );
+        }
+        return [];
+    }
+
+    getLanguageId(): string {
+        return 'javascript';
+    }
+}
