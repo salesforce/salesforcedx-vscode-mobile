41446f2c9534a7ca44a8c5c33d53f38c3f1f2c83
Takashi Arai <t.arai@salesforce.com>
2024-11-11 21:09:36 -0800

    Consume lsp module directly.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,6 +5,6 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 export const MINIMUM_REQUIRED_VERSION_CORE_EXTENSION = '58.4.1';
-export { CORE_EXTENSION_ID, SFDX_PROJECT_FILE } from 'mobile-lsp-server';
+export { CORE_EXTENSION_ID, SFDX_PROJECT_FILE } from '../../lsp/server';
 export const PACKAGE_JSON = 'package.json';
 export const JSON_INDENTATION_SPACES = 2;
