96d71a45ee174db3833e438c26a488bb81b590da
Takashi Arai <t.arai@salesforce.com>
2024-11-19 15:18:46 -0800

    LanguageClient ID. And, use node script to copy yaml file. (#161)
    * LanguageClient ID. And, use node script to copy yaml file.
    
    * Update after feedback.

---
+++ scripts/copyResources.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const projectRoot = path.resolve(__dirname, '..');
+const outDir = path.join(projectRoot, 'out', 'src', 'resources');
+const sourceFile = path.join(projectRoot, 'resources', 'component-experiences.yaml');
+const destFile = path.join(outDir, 'component-experiences.yaml');
+
+fs.mkdirSync(outDir);
+fs.copyFileSync(sourceFile, destFile);
