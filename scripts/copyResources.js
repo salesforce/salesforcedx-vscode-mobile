ba1d65ce7d328d1d60e82d1ba91dd1f03b9f9c2d
Takashi Arai <t.arai@salesforce.com>
2024-11-19 14:03:15 -0800

    Update after feedback.

--- scripts/copyResources.js
+++ scripts/copyResources.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const process = require('process');
-const { execSync } = require('child_process');
 
 const projectRoot = path.resolve(__dirname, '..');
 const outDir = path.join(projectRoot, 'out', 'src', 'resources');
@@ -9,9 +7,4 @@ const sourceFile = path.join(projectRoot, 'resources', 'component-experiences.ya
 const destFile = path.join(outDir, 'component-experiences.yaml');
 
 fs.mkdirSync(outDir);
-
-if (process.platform === 'win32') {    
-    execSync(`copy "${sourceFile}" "${destFile}"`, { stdio: 'inherit', shell: 'cmd.exe' });
-} else {
-    execSync(`cp "${sourceFile}" "${destFile}"`, { stdio: 'inherit' });
-}
+fs.copyFileSync(sourceFile, destFile);
